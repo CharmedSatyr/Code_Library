c6a37cb8d348cfcdf5e1713fe50d07a0c6df62a4
CharmedSatyr <z@charmed.tech>
2017-01-25 22:05:47 +0900

    Completed challenge and included objectives and links

--- freeCodeCamp/Advanced_JS/Make_a_Person.js
+++ freeCodeCamp/Advanced_JS/Make_a_Person.js
@@ -1,27 +1,66 @@
+/*
+
+Make a Person
+
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+
+Run the tests to see the expected output for each method.
+
+The methods that take an argument must accept only one argument and it has to be a string.
+
+These methods must be the only available means of interacting with the object.
+
+Remember to use Read-Search-Ask (https://github.com/FreeCodeCamp/freecodecamp/wiki/FreeCodeCamp-Get-Help) if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+Closures (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures)
+
+Details of the Object Model (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model)
+
+*/
+
+
 var Person = function(firstAndLast) {
 
-  this.getFirstName = function setFirstName(first) {
-    return firstAndLast.split(' ').shift().toString();
-  };
-  
-  this.setFirstName = function getFirstName(first){return first;};
-  
-  this.getLastName = function setLastName(last) {
-    return firstAndLast.split(' ').pop().toString();
-  };
-  
-  this.setLastName = function getLastName(last){return last;};
-  
-  this.getFullName = function setFullName(firstAndLast) {
-    var f = this.getFirstName();
-    var l = this.getLastName();
-    return this.getFirstName() + " " + this.getLastName();
-  };
-  
-  this.setFullName = function getFullName() {};
+  var fullName = firstAndLast;
 
-  
+  //First name
+  this.getFirstName = function() {
+    return fullName.split(' ')[0];
+  };
+
+  this.setFirstName = function(first) {
+    fullName = first + ' ' + fullName.split(' ')[1];
+  };
+
+  //Last name
+  this.getLastName = function() {
+    return fullName.split(' ')[1];
+  };
+
+  this.setLastName = function(last) {
+    fullName = fullName.split(' ')[0] + ' ' + last;
+  };
+
+  //Full name
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFullName = function(both) {
+    fullName = both;
+  };
+
 };
 
 var bob = new Person('Bob Ross');
-bob.setFirstName("Haskell");
+bob.setFirstName('Bob');
+bob.getFirstName();
